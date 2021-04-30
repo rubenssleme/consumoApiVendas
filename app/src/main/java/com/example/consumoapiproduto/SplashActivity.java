9d96a8271c5a1122daeca36d810a8159c72faa35
Rubens Leme <rubenss.leme@gmail.com>
2021-04-30 20:08:13 -0300

    Implementação de uma activity splash

---
+++ app/src/main/java/com/example/consumoapiproduto/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.example.consumoapiproduto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
